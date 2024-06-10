6ad843a27ecc57027c98f90390dace6c8fa0a611
rizqialfani <rizqialfani01@gmail.com>
2024-06-10 21:02:32 +0700

    Develop add todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Todos from './components/Todos';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -32,19 +33,35 @@ function App() {
     setTodos(updatedTodos)
   }
 
-  const toggleDeleted = (e) => {
+  const deleteTodo = (e) => {
     const updatedTodos = todos.filter((todo) => todo.id !== e);
     setTodos(updatedTodos);
+  }
+
+  const addTodo = (todoTitle) => {
+    if (todoTitle === '') {
+      return
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    }
+
+    const updatedTodos = todos.concat(newTodo)
+    setTodos(updatedTodos)
   }
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      {/* Teruskan function toggleCompleted ke component Todos */}
-      <Todos 
-        todos={todos} 
-        toggleCompleted={toggleCompleted} 
-        toggleDeleted={toggleDeleted}
+      {/* Teruskan function addTodo sebagai props */}
+      <TodoForm addTodo={addTodo} /> 
+      <Todos
+        todos={todos}
+        toggleCompleted={toggleCompleted}
+        deleteTodo={deleteTodo}
       />
     </div>
   )
